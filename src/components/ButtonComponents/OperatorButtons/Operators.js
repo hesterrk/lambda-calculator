a1660dd8359e32c2d26a6f0a45dcfa0c5dec0b4b
Hester Knight <hesterknight@Hesters-MacBook-Pro.local>
2019-11-05 22:41:13 +0000

    imported

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -4,6 +4,9 @@ import React from "react";
 
 //Import your array data to from the provided data file
 
+import { operators } from '../data.js';
+
+
 const Operators = () => {
   // STEP 2 - add the imported data to state
   return (
